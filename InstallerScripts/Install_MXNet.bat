cdad7c92bf7e8f51e8480dbf245e9465d6f9bcfa
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-02-20 23:24:20 -0600

    Update Install_MXNet.bat

--- InstallerScripts/Install_MXNet.bat
+++ InstallerScripts/Install_MXNet.bat
@@ -1,11 +1,11 @@
 @cd VapourSynth64
 @echo Installing misc python modules...
-@python.exe -m pip install pyperclip
-@echo Installing MXNet for CUDA 9.2
-@python.exe -m pip install mxnet-cu92 --pre
+@python.exe -m pip install pyperclip --upgrade
+@echo Installing MXNet for CUDA 10/0
+@python.exe -m pip install mxnet-cu100 --pre --upgrade
 @echo  
 @echo Testing MXnet..
 @echo If you get any errors, make sure you installed CUDA and cuDNN!
 @python.exe -c "import mxnet as mx; print(mx.__version__); a = mx.nd.ones((2, 3), mx.gpu()); b = a * 2 + 1; print(b.asnumpy())" -m
 @echo If you got a matrix with 3s, MXNet is working!
-@timeout /t 2
+@timeout /t 3
