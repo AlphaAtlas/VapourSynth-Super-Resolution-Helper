fea0321cd2e89f90bad72e786f4032d93b84787c
AlphaAtlas <brucethemoose34@gmail.com>
2019-05-25 16:18:12 -0500

    Update DownloadEverything.bat

--- Scripts/DownloadEverything.bat
+++ Scripts/DownloadEverything.bat
@@ -1,4 +1,32 @@
 @echo off
+
+echo "Don't click on .bat files you don't trust!""
+echo " "
+echo "This .bat file will download scripts from https://github.com/AlphaAtlas/VapourSynth-Super-Resolution-Helper"
+echo "Which, in turn, will install Nvidia CUDA related files and download archives from other sources"
+echo "into a folder in this directory. Make sure you have a couple of Gigabytes free on C: and in this"
+echo "directory before continuing."
+echo " "
+pause
+cls
+del DownloadCUDA.bat
+del DownloadVapoursynth.bat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AlphaAtlas/VapourSynth-Super-Resolution-Helper/master/Scripts/DownloadCUDA.bat', 'DownloadCUDA.bat')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AlphaAtlas/VapourSynth-Super-Resolution-Helper/master/Scripts/DownloadVapoursynth.bat', 'DownloadVapoursynth.bat')"
+cls
+echo "If you already have all the necessary CUDA and cuDNN dependancies installed, just close this and" 
+echo "run the DownloadVapoursynth.bat file. Otherwise you should uninstall anything that says CUDA" 
+echo "in the Windows Control Panel before continuing. You can also run the DownloadCUDA.bat file to"
+echo "just install/reinstall the Nvidia dependancies" 
+echo " "
+:choice
+set /P c=Are you sure you want to continue[Y/N]?
+if /I "%c%" EQU "Y" goto :install
+if /I "%c%" EQU "N" goto :eof
+goto :choice
+
+:install
+cls
 echo Please give this console window admin rights to install CUDA
 :: BatchGotAdmin
 :-------------------------------------
@@ -25,12 +53,5 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------
 
-
-del DownloadCUDA.bat
-del DownloadVapoursynth.bat
-cls
-echo Here we go!
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AlphaAtlas/vs_mxnet_helper_helper/master/Scripts/DownloadCUDA.bat', 'DownloadCUDA.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AlphaAtlas/vs_mxnet_helper_helper/master/Scripts/DownloadVapoursynth.bat', 'DownloadVapoursynth.bat')"
 start "DownloadCUDA" cmd /c DownloadCUDA.bat
 start "DownloadVapoursynth" cmd /c DownloadVapoursynth.bat
