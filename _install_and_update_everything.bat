cd0503abadb272ed1f85cfda3d1f3e9c5feabfa9
AlphaAtlas <brucethemoose34@gmail.com>
2019-05-26 02:14:25 -0500

    Removed some pauses.

--- _install_and_update_everything.bat
+++ _install_and_update_everything.bat
@@ -1,12 +1,11 @@
 @echo off
 call InstallerScripts/Install_MXNet.bat
-cd %~dp0
 cls
+cd %~dp0
 call InstallerScripts/Install_Neural_Networks.bat
 cd %~dp0
 cd VapourSynth64
 python.exe ../InstallerScripts/Download.py -m
-cd %~dp0
-call extras/_update_plugins_scripts_vsrepo.bat
+cd ../extras
+start "Updating Vapoursynth Filters" _update_plugins_scripts_vsrepo.bat
 echo Done!
-Pause
