5acf2de8e59bd4a16815b128254187ec6d41c216
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-09-29 22:16:02 -0500

    Update _install_and_update_everything.bat

--- _install_and_update_everything.bat
+++ _install_and_update_everything.bat
@@ -5,7 +5,7 @@ cd %~dp0
 call InstallerScripts/Install_Neural_Networks.bat
 cd %~dp0
 cd VapourSynth64
-python.exe ../InstallerScripts/Download.py -m
+python.exe ../InstallerScripts/DownloadMXNetPlugin.py -m
 cd ../extras
 start "Updating Vapoursynth Filters" _update_plugins_scripts_vsrepo.bat
 echo Done!
