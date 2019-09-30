5470e686691f81102065fda0a94649f357ac13db
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-09-29 23:13:04 -0500

    Add MKL only mxnet option

--- InstallerScripts/Install_MXNet.bat
+++ InstallerScripts/Install_MXNet.bat
@@ -1,11 +1,27 @@
 @echo off
 cd VapourSynth64
 echo Installing misc python modules...
-python.exe -m pip install pyperclip --upgrade
-echo Installing MXNet for CUDA 10.0
-python.exe -m pip install mxnet-cu100 --pre --upgrade
+python.exe -m pip install pyperclip psutil --upgrade
+:CUDACheck
+for %%x in (nvcc.exe) do if not [%%~$PATH:x]==[] goto :CUDA
+echo No existing CUDA install detected!
+goto :NoCUDA
+
+:CUDA
+echo Installing MXNet for CUDA 10.1
+python.exe -m pip install mxnet-cu101mkl --pre --upgrade
 timeout /t 3
 cls
+goto :Test
+
+:NoCUDA
+echo Installing CPU only MXNET
+python.exe -m pip install mxnet-mkl --pre --upgrade
+timeout /t 3
+cls
+goto :Test
+
+:Test
 echo Testing MXnet...
 echo If you get any errors, try re-installing CUDA and cuDNN.
 python.exe -c "import mxnet as mx; print(mx.__version__); a = mx.nd.ones((2, 3), mx.gpu()); b = a * 2 + 1; print(b.asnumpy())" -m
