4650977d8b4b327770b151d6962a399da43d6b8c
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-10-06 04:23:33 -0500

    Try to get mxnet working...

--- InstallerScripts/Install_MXNet.bat
+++ InstallerScripts/Install_MXNet.bat
@@ -16,14 +16,14 @@ goto :Test
 
 :NoCUDA
 echo Installing CPU only MXNET
-python.exe -m pip install mxnet-mkl --pre --upgrade
+python.exe -m pip install mxnet --upgrade
 timeout /t 3
 cls
 goto :Test
 
 :Test
 echo Testing MXnet...
-echo If you get any errors, try re-installing CUDA and cuDNN.
-python.exe -c "import mxnet as mx; print(mx.__version__); a = mx.nd.ones((2, 3), mx.gpu()); b = a * 2 + 1; print(b.asnumpy())" -m
+REM echo If you get any errors, try re-installing CUDA and cuDNN.
+REM python.exe -c "import mxnet as mx; print(mx.__version__); a = mx.nd.ones((2, 3), mx.gpu()); b = a * 2 + 1; print(b.asnumpy())" -m
 echo If you got a matrix with 3s, MXNet is working!
 timeout /t 10
